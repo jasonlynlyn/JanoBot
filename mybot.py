20d4a7bec8f39e871425ef21228c76eda928d89c
jasonlynlyn <jason.lyn.lol@gmail.com>
2017-09-28 21:59:52 +1000

    Add files via upload
    day 1 - greetings

---
+++ mybot.py
@@ -0,0 +1,13 @@
+print "Hey Jason, how are you today?"
+
+n = raw_input()
+if n == "Not good.":
+	print "Oh, what's wrong?"
+else:
+	print "What are you doing?"
+
+raw_input() 
+
+print "Ha, nice, that's the way!"
+
+
